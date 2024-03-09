2c095ab439037b65c00a11dfa1d188324ddd5407
karanr3ddy <karanreddy102008@gmail.com>
2024-03-10 04:22:11 +0530

    Upgrade expo SDK to 50 (#33)
    * Upgrade expo SDK to 50
    
    * Revert "Upgrade expo SDK to 50"
    
    This reverts commit ccf8c9dc6e5bd56416d5f02833ef483bddff2e7c.
    
    * Upgrade expo to 50
    
    * Add packages
    *Added @react-navigation/drawer
    *Added eslint-config-prettier and eslint-plugin-prettier which fixes eslint plugin
    *Upgraded expo-three and three packages
    
    * Replace stack with drawer
    *Add hide status bar
    
    * Replace BoxBufferGeometry with BoxGeometry

--- meshs/CubeMesh.tsx
+++ meshs/CubeMesh.tsx
@@ -1,9 +1,9 @@
-import { Mesh, BoxBufferGeometry, MeshStandardMaterial } from 'three';
+import { Mesh, BoxGeometry, MeshStandardMaterial } from 'three';
 
 export default class CubeMesh extends Mesh {
   constructor(x: number, z: number, color: string | number) {
     super(
-      new BoxBufferGeometry(x, 0.2, z),
+      new BoxGeometry(x, 0.2, z),
       new MeshStandardMaterial({
         color: color,
       }),
